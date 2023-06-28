66a3401152fdf2eb3e232c2d9a63e512d5e487d2
Celso Nery <celso.nery@gmail.com>
2023-06-28 19:11:15 -0300

    teste: Change some message in EanUtilTest.php

--- tests/Unit/EanUtilTest.php
+++ tests/Unit/EanUtilTest.php
@@ -9,12 +9,12 @@ class EanUtilTest extends TestCase
 {
     use EanUtil;
 
-    public function testIfEanIsNotValid78912()
+    public function testIfEanIsNotValidWith78912()
     {
         $this->assertFalse($this->isValid("78912"));
     }
 
-    public function testIfEanIsNotValid7891200()
+    public function testIfEanIsNotValidWith7891200()
     {
         $this->assertFalse($this->isValid("7891200"));
     }
@@ -39,7 +39,7 @@ class EanUtilTest extends TestCase
         $this->assertFalse($this->isValid("789127891223"));
     }
 
-    public function testIfEanIsValid7898114289779()
+    public function testIfEanIsValidWith7898114289779()
     {
         $this->assertTrue($this->isValid("7898114289779"));
     }
@@ -74,47 +74,47 @@ class EanUtilTest extends TestCase
         $this->assertEquals("0789811428977", $this->fixLen("789811428977"));
     }
 
-    public function testIfEanEightIsValid50196081()
+    public function testIfEanEightIsValidWith50196081()
     {
         $this->assertTrue($this->isValid("50196081"));
     }
 
-    public function testIfEanEightIsValid50196135()
+    public function testIfEanEightIsValidWith50196135()
     {
         $this->assertTrue($this->isValid("50196135"));
     }
 
-    public function testIfEanEightIsValid50196388()
+    public function testIfEanEightIsValidWith50196388()
     {
         $this->assertTrue($this->isValid("50196388"));
     }
 
-    public function testIfEanEightIsValid50196364()
+    public function testIfEanEightIsValidWith50196364()
     {
         $this->assertTrue($this->isValid("50196364"));
     }
 
-    public function testIfEanEightIsValid50267521()
+    public function testIfEanEightIsValidWith50267521()
     {
         $this->assertTrue($this->isValid("50267521"));
     }
 
-    public function testIfEanEightIsValid50267026()
+    public function testIfEanEightIsValidWith50267026()
     {
         $this->assertTrue($this->isValid("50267026"));
     }
 
-    public function testIfEanEightIsValid78913769()
+    public function testIfEanEightIsValidWith78913769()
     {
         $this->assertTrue($this->isValid("78913769"));
     }
 
-    public function testIfEanEightIsValid78913547()
+    public function testIfEanEightIsValidWith78913547()
     {
         $this->assertTrue($this->isValid("78913547"));
     }
 
-    public function testIfEanEightIsValid78938793()
+    public function testIfEanEightIsValidWith78938793()
     {
         $this->assertTrue($this->isValid("78938793"));
     }
